b03431373cbd548961d885d10b7c46e6236e50a2
Cínthia Eller <cinthiaeller@gmail.com>
2022-07-24 17:50:50 -0300

    Projeto Satander Curso e Mentoria

---
+++ src/Executa.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Executa {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição do curso Java");
+        curso1.setCargaHoraria(20);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("descrição do JS");
+        curso2.setCargaHoraria(18);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
